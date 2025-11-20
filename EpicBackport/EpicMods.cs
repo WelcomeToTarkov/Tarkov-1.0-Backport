94ef82fd7ffde85081c7dc617aefa5ebbc0e2158
eukyre <130890675+eukyre@users.noreply.github.com>
2025-11-20 05:14:56 +0000

    Added new services & changed GUID's

--- EpicBackport/EpicMods.cs
+++ EpicBackport/EpicMods.cs
@@ -8,9 +8,9 @@ namespace EpicBackport;
 
 public record ModMetadata : AbstractModMetadata
 {
-    public override string ModGuid { get; init; } = "com.epicrangetime.backport";
-    public override string Name { get; init; } = "Epics 1.0 Backport";
-    public override string Author { get; init; } = "GrooveypenguinX, EpicRangeTime";
+    public override string ModGuid { get; init; } = "com.wtt.backport";
+    public override string Name { get; init; } = "WTT - 1.0 Backport";
+    public override string Author { get; init; } = "GrooveypenguinX, EpicRangeTime, ProbablyEukyre, Eco";
     public override List<string>? Contributors { get; init; } = null;
     public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
     public override Range SptVersion { get; init; } = new("~4.0.4");
@@ -32,6 +32,10 @@ public class EpicsBackport(
     {
         Assembly assembly = Assembly.GetExecutingAssembly();
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await wttCommon.CustomClothingService.CreateCustomClothing(assembly);
+        await wttCommon.CustomBotLoadoutService.CreateCustomBotLoadouts(assembly);
+        await wttCommon.CustomVoiceService.CreateCustomVoices(assembly);
+        //await wttCommon.CustomRigLayoutService.CreateRigLayouts(assembly);
         //await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
         //await wttCommon.CustomWeaponPresetService.CreateCustomWeaponPresets(assembly);
         await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
