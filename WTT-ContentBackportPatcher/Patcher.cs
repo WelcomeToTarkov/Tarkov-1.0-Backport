bb9ac6e0312637ab3ee1cf7789b8eb4793a407ac
GrooveypenguinX <jakechase2110@gmail.comX>
2026-04-07 11:21:54 -0400

    backport patcher

---
+++ WTT-ContentBackportPatcher/Patcher.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+
+namespace PunisherBossModPreloader
+{
+
+    [BepInPlugin("com.wtt.contentbackport", "Content Backport Preloader Patch", "1.0.7")]
+    public class Patcher : BaseUnityPlugin
+    {
+
+        public static Patcher Instance { get; private set; }
+
+        public void Awake()
+        {
+            Patcher.Instance = this;
+        }
+    }
+}
